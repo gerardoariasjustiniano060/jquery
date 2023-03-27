69ec80ba56e5d1f94a0229997eed52eb22d3b196
gerardo9999 <gerardoariasjustiniano060@gmail.com>
2023-03-26 21:39:37 -0400

    add template principal

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -4,15 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Document</title>
 
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/css/adminlte.min.css">
-
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-qU6zOft+VvQON/z5Y5RE5I5lLl8BJShFa6DZ37zV9AWR0A0fVvTxMhY3gV4Q4G43Ny7VvWykPLrXQrFlgRUZBg==" crossorigin="anonymous" /> --}}
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.3/css/all.min.css">
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    @include('includes.links')
 </head>
 
     <body class="hold-transition sidebar-mini layout-fixed">
@@ -24,13 +19,11 @@
             
             @include('template.navbar')
 
-           @include('template.sidebar')
-
+            @include('template.sidebar')
 
             <div class="content-wrapper">
                 @yield('content')
             </div>
-
 
 
             <footer class="main-footer">
@@ -40,14 +33,10 @@
                     <b>Version</b> 3.2.0
                 </div>
             </footer>
-
-           
-
-
+            
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.1/dist/js/adminlte.min.js"></script>
-        <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
-        @stack('index')
+        
+        @include('includes.scripts')
+        @stack('cliente-component')
     </body>
 </html>
